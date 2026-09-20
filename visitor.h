1a79e1037c56aefa833c881b5db5c0e1e223d49f
Javier Olivares <165913434+Javier1308@users.noreply.github.com>
2026-09-20 00:39:32 +0000

    Implementar gramática de Semana 6: relacionales, lógicos, booleanos y switch
    Extiende el intérprete con la gramática completa de Sem6_PruebaCode: RelExp,
    LAndExp, LOrExp, literales true/false, sentencia switch/case/break/default/
    endswitch y while-do-endwhile, siguiendo la estructura y convenciones del
    Laboratorio 4 (scanner, token, parser recursivo descendente, AST y Visitor).
    Se valida con run_all_inputs.py sobre los 10 inputs de prueba.

---
+++ visitor.h
@@ -0,0 +1,85 @@
+#ifndef VISITOR_H
+#define VISITOR_H
+#include "ast.h"
+#include <list>
+#include <unordered_map>
+
+class BinaryExp;
+class NumberExp;
+class BoolExp;
+class SqrtExp;
+class IdExp;
+class Program;
+class PrintStatement;
+class AssignStatement;
+class BreakStatement;
+class Body;
+class IfStatement;
+class ElifStatement;
+class DoWhileStatement;
+class WhileStatement;
+class SwitchStatement;
+
+class Visitor {
+public:
+    virtual int visit(BinaryExp* exp) = 0;
+    virtual int visit(NumberExp* exp) = 0;
+    virtual int visit(BoolExp* exp) = 0;
+    virtual int visit(SqrtExp* exp) = 0;
+    virtual int visit(Program* p) = 0;
+    virtual int visit(IdExp* exp) = 0;
+    virtual int visit(PrintStatement* stm) = 0;
+    virtual int visit(AssignStatement* stm) = 0;
+    virtual int visit(BreakStatement* stm) = 0;
+    virtual int visit(Body* stm) = 0;
+    virtual int visit(IfStatement* stm) = 0;
+    virtual int visit(ElifStatement* stm) = 0;
+    virtual int visit(DoWhileStatement* stm) = 0;
+    virtual int visit(WhileStatement* stm) = 0;
+    virtual int visit(SwitchStatement* stm) = 0;
+};
+
+class PrintVisitor : public Visitor {
+public:
+
+    int visit(BinaryExp* exp) override;
+    int visit(NumberExp* exp) override;
+    int visit(BoolExp* exp) override;
+    int visit(SqrtExp* exp) override;
+    int visit(IdExp* exp) override;
+    int visit(Program* p) override;
+    int visit(PrintStatement* stm) override;
+    int visit(AssignStatement* stm) override;
+    int visit(BreakStatement* stm) override;
+    int visit(Body* stm) override;
+    int visit(IfStatement* stm) override;
+    int visit(ElifStatement* stm) override;
+    int visit(DoWhileStatement* stm) override;
+    int visit(WhileStatement* stm) override;
+    int visit(SwitchStatement* stm) override;
+    void imprimir(Program* program);
+};
+
+class EVALVisitor : public Visitor {
+public:
+    unordered_map<string,int> memoria;
+    int visit(BinaryExp* exp) override;
+    int visit(NumberExp* exp) override;
+    int visit(BoolExp* exp) override;
+    int visit(SqrtExp* exp) override;
+    int visit(IdExp* exp) override;
+    int visit(Program* p) override;
+    int visit(PrintStatement* stm) override;
+    int visit(AssignStatement* stm) override;
+    int visit(BreakStatement* stm) override;
+    int visit(Body* stm) override;
+    int visit(IfStatement* stm) override;
+    int visit(ElifStatement* stm) override;
+    int visit(DoWhileStatement* stm) override;
+    int visit(WhileStatement* stm) override;
+    int visit(SwitchStatement* stm) override;
+    void interprete(Program* program);
+};
+
+
+#endif // VISITOR_H
